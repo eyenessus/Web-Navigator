b98d72f193ce4488ac2522b5003205c1ab3b51e3
Emerson S <eyenessus@gmail.com>
2024-03-15 21:49:08 -0300

    Adicionar nova pagina

--- script.ts
+++ script.ts
@@ -1,17 +1,17 @@
-// var prompt = require('prompt-sync')();
+var prompt = require('prompt-sync')();
 // ------------------------------
 // Initialization
 
 import Stack from "./Structure/Stack/Stack";
 
 // ------------------------------
-const currentPage = 'Google'
-const backPages = new Stack()
-const nextPages= new Stack()
+let currentPage:string = 'Google'
+let backPages = new Stack()
+let nextPages= new Stack()
 // ------------------------------
 // Helper Functions
 // ------------------------------
-const showCurrentPage = (action)=>{
+const showCurrentPage = (action:string)=>{
    console.log(`\n${action}`);
   console.log(`Current page = ${currentPage}`);
   console.log('Back page = ', backPages.peek());
@@ -26,6 +26,14 @@ const nextInfo = 'N|n for next page';
 const quitInfo = 'Q|q for quit';
 const question = 'Where would you like to go today? '
 
+const newPage = (page:string)=>{
+  backPages.push(currentPage)
+  currentPage = page
+  while(!nextPages.isEmpty()){
+    nextPages.pop()
+  }
+  showCurrentPage(currentPage)
+}
 // ------------------------------
 // User Interface Part 1
 // ------------------------------
