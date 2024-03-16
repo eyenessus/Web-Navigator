5cf30e8564f8494aebb3c522b9878ceaf5c84879
Emerson S <eyenessus@gmail.com>
2024-03-15 21:41:29 -0300

    Adicionado pagina default e configurações iniciais

--- script.ts
+++ script.ts
@@ -1,12 +1,22 @@
 // var prompt = require('prompt-sync')();
 // ------------------------------
 // Initialization
-// ------------------------------
 
+import Stack from "./Structure/Stack/Stack";
+
+// ------------------------------
+const currentPage = 'Google'
+const backPages = new Stack()
+const nextPages= new Stack()
 // ------------------------------
 // Helper Functions
 // ------------------------------
-
+const showCurrentPage = (action)=>{
+   console.log(`\n${action}`);
+  console.log(`Current page = ${currentPage}`);
+  console.log('Back page = ', backPages.peek());
+  console.log('Next page = ', nextPages.peek());
+}
 /*
  * The following strings are used to prompt the user
  */
